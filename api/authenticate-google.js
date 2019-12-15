27f781ab38e63a545e8bce67ee54978c82bf572e
Rails Udemy Course <enginbulut@live.com>
2019-12-15 16:02:50 +0300

    added google auth

---
+++ api/authenticate-google.js
@@ -0,0 +1,55 @@
+/**
+ * Route: GET /auth
+ */
+
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+
+const jwtDecode = require("jwt-decode");
+const util = require("./util.js");
+
+const cognitoIdentity = new AWS.CognitoIdentity();
+const idetityPoolId = process.ENV.COGNITO_IDENTITY_POOL_ID;
+
+exports.handler = async event => {
+  try {
+    let id_token = util.getIdToken(event.headers);
+
+    let params = {
+      IdentityPoolId: idetityPoolId,
+      Logins: {
+        "accounts.google.com": id_token
+      }
+    };
+
+    let data = await cognitoIdentity.getId(params).promise();
+
+    params = {
+      IdentityID: data.IdentityID,
+      Logins: {
+        "accounts.google.com": id_token
+      }
+    };
+
+    data = await cognitoIdentity.getCredentialsForIdentity(params).promise();
+
+    let decoded = jwtDecode(id_token);
+    data.user_name = decoded.name;
+
+    return {
+      statusCode: 200,
+      headers: util.getResponseHeaders(),
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      headers: util.getResponseHeaders(),
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error"
+      })
+    };
+  }
+};
